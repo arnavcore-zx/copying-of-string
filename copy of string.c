8a70a803be1c409c824de05b28c0303edf6b69c6
arnavcore-zx <srisantarnav@gmail.com>
2026-09-16 22:32:44 +0530

    Add files via upload

---
+++ copy of string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char source[100], destination[100];
+    int i = 0;
+
+    printf("Enter source string: ");
+    gets(source);
+
+    while (source[i] != '\0')
+    {
+        destination[i] = source[i];
+        i++;
+    }
+
+    destination[i] = '\0';
+
+    printf("Copied string: %s", destination);
+
+    return 0;
+}
